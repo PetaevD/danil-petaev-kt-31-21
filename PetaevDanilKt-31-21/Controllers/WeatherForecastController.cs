2f134fb95df1f8a0764d42272b3dcd12b6ace451
sm1leProgrammer <petaevd@yandex.ru>
2024-09-14 11:35:37 +0300

    -add logger -add new method

--- PetaevDanilKt-31-21/Controllers/WeatherForecastController.cs
+++ PetaevDanilKt-31-21/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace PetaevDanilKt_31_21.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -21,6 +21,8 @@ namespace PetaevDanilKt_31_21.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
@@ -28,6 +30,16 @@ namespace PetaevDanilKt_31_21.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
         }
     }
 }
